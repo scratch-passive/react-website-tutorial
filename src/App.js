c10943a735a039694d3f78ccbcbb985718068f90
scratch-passive <isond@mymacewan.ca>
2023-11-06 21:10:50 -0700

    Adding css and functionality to Button component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-          <Routes>
-            <Route path='/' exact />
-          </Routes>
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
       </Router>
     </>
   );
